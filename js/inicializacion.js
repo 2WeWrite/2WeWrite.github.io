67561be0103bd1a6415da38aefd00b2a1de20c8e
2WeWrite <96317625+2WeWrite@users.noreply.github.com>
2021-12-17 16:21:38 -0600

    Update inicializacion.js

--- js/inicializacion.js
+++ js/inicializacion.js
@@ -1,11 +1,10 @@
 /*  Inicializa Firebase con la configuración del proyecto. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyAXoqlSzgA2an2L6uSSOtXFX4Q4zHr0Mvc",
-    authDomain: "ecologaroom.firebaseapp.com",
-    projectId: "ecologaroom",
-    storageBucket: "ecologaroom.appspot.com",
-    messagingSenderId: "509252941570",
-    appId: "1:509252941570:web:e651c919f5d0902470741b",
-    measurementId: "G-45R2RM4HV8"
+    apiKey: "AIzaSyA8PXn1cFduaH0mtKSVKqW17DB32M7e7N4",
+  authDomain: "wewrite-2c80c.firebaseapp.com",
+  projectId: "wewrite-2c80c",
+  storageBucket: "wewrite-2c80c.appspot.com",
+  messagingSenderId: "748253492443",
+  appId: "1:748253492443:web:4eba8e1a973cfc122dc8fe"
 });
