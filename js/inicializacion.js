b2e66c36a40b8fa85589fe0da412c5389f11dd46
2WeWrite <2WeWrite@gmail.com>
2021-12-17 20:55:51 -0600

    Conexión a Firebase

--- js/inicializacion.js
+++ js/inicializacion.js
@@ -1,10 +1,11 @@
 /*  Inicializa Firebase con la configuración del proyecto. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyA8PXn1cFduaH0mtKSVKqW17DB32M7e7N4",
+  apiKey: "AIzaSyA8PXn1cFduaH0mtKSVKqW17DB32M7e7N4",
   authDomain: "wewrite-2c80c.firebaseapp.com",
   projectId: "wewrite-2c80c",
   storageBucket: "wewrite-2c80c.appspot.com",
   messagingSenderId: "748253492443",
-  appId: "1:748253492443:web:4eba8e1a973cfc122dc8fe"
+  appId: "1:748253492443:web:4eba8e1a973cfc122dc8fe",
+  measurementId: "G-3XMM73NZKF"
 });
